25b99259ab125629d513ce89991574cebc560807
Endashaw <Leul.Endashaw@McKesson.com>
2022-10-17 10:36:08 -0400

    updated

--- fastapisqlGit.py
+++ fastapisqlGit.py
@@ -5,6 +5,8 @@ import uvicorn, os
 from pydantic import BaseModel
 from typing import Optional
 
+from DBConnection import DBConnection
+
 app = FastAPI()
 
 
@@ -18,9 +20,9 @@ class Person(BaseModel):
     legacy_account: str
 
 
-# replace the db_host with public IP for local and the private one for the Cloud
+# DB connection
 
-con = pymysql.connect(host="{{db_host}}", user="{{db_root}}", password="{{db_password}}", db="db_name")
+con = DBConnection.get_db_connection()
 cursor = con.cursor()
 
 with open('Customer.json', 'r') as f:
@@ -34,106 +36,95 @@ def get_customer(customer_account_number: int):
     return person[0] if len(person) > 0 else {}
 
 
-# Searches for customer
-@app.get("/search", status_code=200)
-def search_person(account_number: Optional[int] = Query(None, title="account_number",
-                                                        description="The account number to filter by"),
-                  account_type: Optional[str] = Query(None, title="account_type",
-                                                      description="The account type to filter by")):
-    customer1 = [c for c in customer if c['account_number'] == account_number]
-    if account_type is None:
-        if account_number is None:
-            return customer
-        else:
-            return customer1
-    else:
-        customer2 = [c for c in customer if account_type.lower() in c['account_type'].lower()]
-        if account_number is None:
-            return customer2
-        else:
-            combined = [c for c in customer1 if c in customer2]
-            return combined
+# Searches for customer from DB
+# Get one Customer from the DB
+@app.get('/search/{customer_account_number}')
+def get_one_db_customer(customer_account_number: int):
+    query = "SELECT * FROM customer where account_number={0}".format(customer_account_number)
+    print(query)
+    cursor.execute(query)
+    row_headers = [x[0] for x in cursor.description]
+    result = cursor.fetchone()
+    person_json = []
+    person_json.append(dict(zip(row_headers, result)))
+    return person_json
 
 
-# Add one customer to the DB
-@app.post('/addCustomer')
-def add_customer(person: Person):
-    new_customer = {
-        "account_number": person.account_number,
-        "account_type": person.account_type,
-        "customer_number": person.customer_number,
-        "account_record_date": person.account_record_date,
-        "status_indicator": person.status_indicator,
-        "invoice_type": person.invoice_type,
-        "legacy_acoount": person.legacy_account
-    }
-
-    account_number = new_customer.get("account_number")
-    account_type = new_customer.get("account_type")
-    customer_number = new_customer.get("customer_number")
-    account_record_date = new_customer.get("account_record_date")
-    status_indicator = new_customer.get("status_indicator")
-    invoice_type = new_customer.get("invoice_type")
-    legacy_account = new_customer.get("legacy_account")
-    insert_query = "INSERT INTO customer (account_number,account_type,customer_number,account_record_date,status_indicator,invoice_type,legacy_account) VALUES({0},'{1}',{2},{3},'{4}','{5}','{6}') ".format(
-        account_number, account_type, customer_number, account_record_date, status_indicator, invoice_type,
-        legacy_account)
-    print(insert_query)
-    cursor.execute(insert_query)
-    con.commit()
-    con.close()
-
-    return
+# Get all customers from DB
+@app.get('/dbcustomer')
+def get_all_customers_from_db():
+    query = "SELECT * FROM customer"
+    print(query)
+    cursor.execute(query)
+    row_headers = [x[0] for x in cursor.description]
+    results = cursor.fetchall()
+    person_json = []
+    for result in results:
+        person_json.append(dict(zip(row_headers, result)))
+    return person_json
 
 
-@app.put('/changeCustomer', status_code=204)
-def change_customer(person: Person):
-    new_customer = {
-        "account_number": person.account_number,
-        "account_type": person.account_type,
-        "customer_number": person.customer_number,
-        "account_record_date": person.account_record_date,
-        "invoice_type": person.invoice_type,
-        "legacy_account": person.legacy_account
-    }
-
-    customer_list = [c for c in customer if c['account_number'] == person.account_number]
-    if len(customer_list) > 0:
-        customer.remove(customer_list[0])
-        customer.append(new_customer)
-        with open('Customer.json', 'w') as f:
-            json.dump(customer, f)
-        return new_customer
-    else:
-        return HTTPException(status_code=404, detail=f"Customer with id {person.account_number} does not Exit!")
-
-
-# Get all customres from the JSON file
+# Get all customers from the JSON file
 @app.get('/customer')
 def get_all_customers():
     person = [c for c in customer]
     return person
 
 
-# Get all customers from DB
-@app.get('/dbcustomer')
-def get_all_db_customers():
-    query = "SELECT * FROM customer"
-    print(query)
-    cursor.execute(query)
-    person = cursor.fetchall()
-    con.close()
-    return person
+#
+# @app.get("/search", status_code=200)
+# def search_person(account_number: Optional[int] = Query(None, title="account_number",
+#                                                         description="The account number to filter by"),
+#                   account_type: Optional[str] = Query(None, title="account_type",
+#                                                       description="The account type to filter by")):
+#     customer1 = [c for c in customer if c['account_number'] == account_number]
+#     if account_type is None:
+#         if account_number is None:
+#             return customer
+#         else:
+#             return customer1
+#     else:
+#         customer2 = [c for c in customer if account_type.lower() in c['account_type'].lower()]
+#         if account_number is None:
+#             return customer2
+#         else:
+#             combined = [c for c in customer1 if c in customer2]
+#             return combined
 
-# Get one Customer from the DB
-@app.get('/dbcustomer/{customer_account_number}')
-def get_one_db_customers(customer_account_number: int):
-    query = "SELECT * FROM customer where customer_account_number={0}".format(customer_account_number)
+
+# Add one customer to the DB
+
+# Add one customer information to the Database
+@app.post('/addCustomer')
+def add_one_customer(person: Person):
+
+    insert_query = "INSERT INTO customer (account_number,account_type,customer_number,account_record_date,status_indicator,invoice_type,legacy_account) VALUES({0},'{1}',{2},{3},'{4}','{5}','{6}') ".format(
+        person.account_number, person.account_type, person.customer_number, person.account_record_date, person.status_indicator, person.invoice_type,person.legacy_account)
+    print(insert_query)
+    cursor.execute(insert_query)
+    con.commit()
+
+    return
+
+
+@app.put('/changeCustomer', status_code=204)
+def change_customer(person: Person):
+
+    update_query="update customer set account_number={0}, account_type='{1}', customer_number={2},account_record_date={3},status_indicator='{4}',invoice_type='{5}',legacy_account='{6}' Where account_number={0};".format(
+        person.account_number, person.account_type, person.customer_number, person.account_record_date, person.status_indicator, person.invoice_type, person.legacy_account)
+    print(update_query)
+    cursor.execute(update_query)
+    con.commit()
+
+
+# Delete for customer from DB
+@app.delete('/delete/{customer_account_number}')
+def delete_a_customer_from_db(customer_account_number: int):
+    query = "DELETE FROM customer WHERE account_number={0}".format(customer_account_number)
     print(query)
     cursor.execute(query)
-    person = cursor.fetchone()
-    con.close()
-    return person
+    con.commit()
+
 
 # Delete JSON file entries
 @app.delete('/deleteCustomer/{customer_account_number}', status_code=204)
@@ -150,9 +141,9 @@ def delete_person(customer_account_number: int):
 
 # Loads all the customer list from the JSON onto the SQL
 @app.post('/loadCustomer')
-def load_customer():
+def load_all_customer_to_db():
     person = [c for c in customer]
-    #  Loading the list onto the DB
+    #  Loading to the DB
     for customersql in person:
         account_number = customersql.get("account_number")
         account_type = customersql.get("account_type")
@@ -167,7 +158,6 @@ def load_customer():
         print(insert_query)
         cursor.execute(insert_query)
     con.commit()
-    con.close()
 
 
 if __name__ == "__main__":
